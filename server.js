e0efacbd533dfb64deebb5b5487f739c94969bb9
GUILHERME DALSASSO MACHADO <guilherme_machado18@estudante.sesisenai.org.br>
2026-03-11 12:12:40 -0300

    Post funcionando

--- server.js
+++ server.js
@@ -23,31 +23,30 @@ servidor.post('/filmes', async (request, reply) => {
  if (!titulo || !genero || !ano_lancamento || !diretor) {
  return reply.status(400).send({ error: 'Algum dos dados são Inválidos!' })
  }
- await sql.query('INSERT INTO usuario (titulo, genero, ano_lancamento, diretor) VALUES ($1, $2, $3, $4)', [titulo, genero, ano_lancamento, diretor])
+ await sql.query('INSERT INTO filmes (titulo, genero, ano_lancamento, diretor) VALUES ($1, $2, $3, $4)', [titulo, genero, ano_lancamento, diretor])
  return reply.status(201).send({ mensagem: "Filme cadastrado no catálogo!" })
 })
 
 // Editar usuário existente
 servidor.put('/filmes/:id', async (request, reply) => {
  const { id } = request.params
- const { nome, senha } = request.body
+ const { titulo, genero, ano_lancamento, diretor } = request.body
  if (!nome || !senha) {
- return reply.status(400).send({ error: 'Nome ou senha inválidos!' })
+ return reply.status(400).send({ error: 'Algum dos dados são inválidos!' })
  }
- const busca = await sql.query('SELECT * FROM usuario WHERE id = $1', [id])
+ const busca = await sql.query('SELECT * FROM filmes WHERE id = $1', [id])
 
  if (busca.rows.length === 0) {
- return reply.status(404).send({ error: 'Usuário não encontrado!' })
+ return reply.status(404).send({ error: 'Filme não encontrado!' })
  }
- await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [nome,
-senha, id])
- return { mensagem: 'Usuário alterado com sucesso!' }
+ await sql.query('UPDATE usuario SET titulo = $1, genero = $2, ano_lancamento = $3, diretor = $4 WHERE id = $5', [titulo, genero, ano_lancamento, diretor, id])
+ return { mensagem: 'Filme alterado com sucesso!' }
 })
 
 // Deletar usuário
 servidor.delete('/filmes/:id', async (request, reply) => {
  const { id } = request.params
- await sql.query('DELETE FROM usuario WHERE id = $1', [id])
+ await sql.query('DELETE FROM filmes WHERE id = $1', [id])
  return reply.status(204).send()
 })
 
